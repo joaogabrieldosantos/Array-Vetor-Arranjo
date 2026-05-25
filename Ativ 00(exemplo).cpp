89066348f9edb3b92f62ae8a36715353ceec24c7
joaogabrieldosantos <joaogabrieldosantos0901@gmail.com>
2026-05-25 14:13:34 -0300

    Update Ativ 00(exemplo).cpp

--- Ativ 00(exemplo).cpp
+++ Ativ 00(exemplo).cpp
@@ -18,3 +18,38 @@ for(int i = 0; i < 4; i++){
 return 0;
 
 }
+
+______________________________/0/_____________________________________ /0/___________________________________
+
+#include <iostream>
+using namespace std;
+
+int main(){
+float * notas;
+int n;
+int soma = 0;
+
+    cout << "Me informe quantas notas: " << endl;
+    cin >> n;
+    
+    notas = new float [n];
+    
+for (int i = 0; i < n; i++){
+	
+    
+	cout << "Sua Nota " << i+1 << ": ";
+	
+	cin >> notas[i];
+	
+	cout << notas[i] << endl;
+	
+    soma = soma + notas[i];
+	
+	}
+	
+	float div = soma/ n;
+	
+	cout << div << endl;
+	
+return 0;
+}
